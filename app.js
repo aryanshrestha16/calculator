0c174213838baf06b77e65763b23eeac2c3cbb6c
Aryan Shrestha <aryanshrestha3810@gmail.com>
2024-03-17 17:33:11 -0400

    Add keyboard shortcut

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 "use strict"
 let displayValue = "";
-function add (x,y) {
-    return x + y;
-}
+let result = 0;
+let operand = "";
+const displayScreen = document.getElementById("display");
+updateActiveButton();
 
-function subtract(x,y){
-    return x - y ;
-}
-
-function multiply(x,y){
-    return x * y ;
-}
-
-function divide(x,y){
-    return x/y ;
+function operate(operator, x, y) {
+    switch(operator) {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            if (y === 0) {
+                return "Error: Division by zero";
+            }
+            return x / y;
+        default:
+            return "Error: Invalid operator";
+    }
 }
 
 function displayNumber(number){
+    removeActivebuttons();
     const displayScreen = document.getElementById("display");
-    displayValue += number;
+    if (displayValue === "0"){
+        displayValue = String(number);
+    }
+    else
+    {
+    displayValue = displayValue.concat(number);
+    }
     displayScreen.value = displayValue;
 }
 
@@ -28,7 +42,66 @@ function clearScreen(){
 }
 
 function deleteLast(){
-    let newDisplay = displayValue.slice(0,-1);
-    displayValue = "";
-    displayNumber(newDisplay);
+    displayValue = displayValue.slice(0, -1);
+    if (displayValue === "") {
+        displayScreen.value = "0";
+    }
+    else {
+    displayScreen.value = displayValue;
+    }
 }
+
+function displayResult(){
+    result = operate(operand, result, parseFloat(displayValue));
+    if (typeof result === "number") {
+        displayValue = result.toString();
+        displayScreen.value = displayValue;
+    } else {
+        console.error(result);
+    }
+}
+
+function percentsign(){
+    if (displayValue != ""){
+        displayValue = parseFloat(displayValue) / 100;
+        result = displayValue
+        displayScreen.value = result;
+    }
+}
+
+function addOperand(sign){
+    if (operand !== "") {
+        displayResult();
+    }
+    operand = sign;
+    result = parseFloat(displayValue);
+    displayValue = "";
+}
+
+function acbutton(){
+    displayValue = "";
+    operand = "";
+    result = 0;
+    removeActivebuttons();
+    displayNumber(0);
+}
+
+function updateActiveButton() {
+    let buttons = document.querySelectorAll(".operand");
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            let current = document.querySelector(".active");
+            if (current) {
+                current.classList.remove("active");
+            }
+            button.classList.add("active");
+        });
+    });
+}
+
+function removeActivebuttons(){
+    let current = document.querySelector(".operand.active");
+        if (current) {
+            current.classList.remove("active");
+        }
+    }
